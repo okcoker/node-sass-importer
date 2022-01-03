f967949d629dca2ed6db3c6bf0ecb647aabd0bd3
Sean Coker <1030830+okcoker@users.noreply.github.com>
2022-01-03 13:03:02 -0500

    Allow imports with extension
    Doing something like `@import "module.scss"` would fail without this

--- src/node-sass-importer.js
+++ src/node-sass-importer.js
@@ -96,6 +96,7 @@ function getRuntime(context) {
                     '',
                 ],
                 fileExtensions: [
+                    '',
                     '.scss',
                     '/_index.scss',
                     '.css'
